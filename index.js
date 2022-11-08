3062b9b508544aa7520b77fb1d27dae4d1f58714
Fahim Shahriar Akash <fahimshahriarakash03@gmail.com>
2022-11-08 13:21:09 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("roots server is running");
 });
+
+//user: dbuser8
+//pass: QmzV2Af2fNdOIXAP
+
+const uri =
+  "mongodb+srv://dbuser8:QmzV2Af2fNdOIXAP@cluster0.4r8e4ne.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+// client.connect((err) => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+async function run() {
+  try {
+    const serviceCollection = client.db("rootsServices").collection("services");
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const result = await cursor.limit(3).toArray();
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch((error) => console.log(error));
 
 app.listen(port, () => {
   console.log(`Api is running on port ${port}`);
