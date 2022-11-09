fb74b52c66834adceb73bb2b9ca9aae8362402be
Fahim Shahriar Akash <fahimshahriarakash03@gmail.com>
2022-11-09 14:31:19 +0600

    update review added

--- index.js
+++ index.js
@@ -74,6 +74,26 @@ async function run() {
       res.send(result);
     });
 
+    app.put("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const review = req.body;
+      const option = { upsert: true };
+      const updatedReview = {
+        $set: {
+          Reviewer: review.Reviewer,
+          email: review.email,
+          message: review.message,
+        },
+      };
+      const result = await reviewCollection.updateOne(
+        filter,
+        updatedReview,
+        option
+      );
+      res.send(result);
+    });
+
     app.delete("/reviews/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
